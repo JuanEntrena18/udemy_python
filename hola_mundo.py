36a6607401f5d0a156fe2b081a39147e0f53ac0e
Juan Entrena Garrido <jentrenagarr@protonmail.com>
2025-11-12 18:24:34 +0100

    Num pares

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,17 @@
 #Primer código del curso
 print ("Hola Mundo")
+
+# Esto es una variable
+
+A = "Letra"
+B = 20
+print ("La palabra es", A, "y el número es", B)
+
+# Imprime los números pares desde el 0 hasta n
+n = int(input("Elige un número"))
+for i in range (n + 1):
+    if i % 2 == 0:
+        print(i)
+    else:
+        print (f"{i} (Impar)")
+
