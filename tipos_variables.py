f4f49f0ca7e5c19368e43d05b5ed096a270fb1a1
Juan Entrena Garrido <jentrenagarr@protonmail.com>
2025-11-12 18:30:06 +0100

    variables

---
+++ tipos_variables.py
@@ -0,0 +1,15 @@
+# Enteros int
+x = 5
+# Flotantes float
+y = 0.5
+# Se pueden sumar variables int y float
+suma = x + y
+print (suma)
+# Datos booleanos
+es_verdadero = True
+es_falso = False
+# String, va entre comillas
+string = "Hola"
+# Convertir string a int
+a = "6"
+convertir = int(a)
